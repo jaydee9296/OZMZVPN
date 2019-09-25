5085746220e5897c1f0b89beaa34c82142a5e683
John Dave Balingit <jaydee9296@users.noreply.github.com>
2019-09-25 12:02:02 +0800

    Add files via upload

---
+++ OZMZVPN-SSL.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+if [[ "$EUID" -ne 0 ]]; then
+    echo -e "Sorry, you need to run this as root!"
+    exit 2
+fi
+
+echo -e "\t-------------------------------------------------------\n"
+echo -e "\t|   SSL over SSH & OpenVPN Script by Edmar Hub :]x]   |\n"
+echo -e "\t-------------------------------------------------------\n"
+
+read -n1 -r -p "Press any key to start ..."
+clear
+
+# Default Stunnel Value
+COUNTRY="PH"
+STATE="Misamis Occidental"
+CITY="Ozamiz"
+ORGANIZATION="JayDee Solutions"
+ORGANIZATIONAL_UNIT=""
+COMMON_NAME="OZMZVPN"
+EMAIL="johndave10010001010000@gmail.com"
+PASSWORD="JohnDave.me9296"
+
+# Default SSH, OpenVPN, Stunnel Ports values
+DEFAULT_SSH_PORT="22"
+DEFAULT_SSH_SSL_PORT="443"
+DEFAULT_OPENVPN_PORT="1194"
+DEFAULT_OPENVPN_SSL_PORT="587"
+
+read -p "Enter SSH Port: " -e -i $DEFAULT_SSH_PORT SSH_PORT
+while [[ "$SSH_PORT" == "" ]]
+do
+    read -p "Enter SSH Port:: " -e -i $DEFAULT_SSH_PORT SSH_PORT
+done
+
+read -p "Enter SSH SSL Port: " -e -i $DEFAULT_SSH_SSL_PORT SSH_SSL_PORT
+while [[ "$SSH_SSL_PORT" == "" ]]
+do
+    read -p "Enter SSH SSL Port: " -e -i $DEFAULT_SSH_SSL_PORT SSH_SSL_PORT
+done
+
+read -p "Enter OpenVPN Port: " -e -i $DEFAULT_OPENVPN_PORT OPENVPN_PORT
+while [[ "$OPENVPN_PORT" == "" ]]
+do
+    read -p "Enter OpenVPN Port: " -e -i $DEFAULT_OPENVPN_PORT OPENVPN_PORT
+done
+
+read -p "Enter OpenVPN SSL Port: " -e -i $DEFAULT_OPENVPN_SSL_PORT OPENVPN_SSL_PORT
+while [[ "$OPENVPN_SSL_PORT" == "" ]]
+do
+    read -p "Enter OpenVPN SSL Port: " -e -i $DEFAULT_OPENVPN_SSL_PORT OPENVPN_SSL_PORT
+done
+
+apt-get -y update
+apt-get -y install stunnel4 openssl
+
+echo "cert = /etc/stunnel/stunnel.pem
+[ssh] 
+accept = $SSH_SSL_PORT
+connect = 127.0.0.1:$SSH_PORT
+
+[openvpn]
+accept = $OPENVPN_SSL_PORT
+connect = 127.0.0.1:$OPENVPN_PORT" > /etc/stunnel/stunnel.conf
+
+openssl genrsa -out key.pem 2048 && openssl req -new -x509 -key key.pem -out cert.pem -days 1095 -passin pass:$PASSWORD \
+    -subj "/C=$COUNTRY/ST=$STATE/L=$CITY/O=$ORGANIZATION/OU=$ORGANIZATIONAL_UNIT/CN=$COMMON_NAME/emailAddress=$EMAIL"
+
+cat key.pem cert.pem > /etc/stunnel/stunnel.pem
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+
+echo "Successfully installed SSL over SSH & OpenVPN!"
